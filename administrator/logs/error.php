024da40bc8b5aaf747297e9d5c811b9de5d9700c
JoseVG1 <1530209@upv.edu.mx>
2019-01-24 22:58:26 -0600

    primer commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-24 19:07:03 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-24T19:07:03+00:00	INFO ::1	joomlafailure	Empty password not allowed.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-23 02:10:43 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-23T02:10:43+00:00	INFO 127.0.0.1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
